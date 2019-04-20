05c07cf1ccbd8938dc5de407b30c2bde9327f337
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 18:39:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else 
-    string = "The line is currently: "
+    string = "The line is currently:"
     array.each_with_index do |name, index|
       string << " #{index + 1}. #{name}"
     end
