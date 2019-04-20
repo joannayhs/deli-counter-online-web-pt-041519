919735a40f3195afdb718225b13fccfdc324ef11
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 18:35:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
     string = "The line is currently: "
     array.each_with_index do |name, index|
       string << "#{index + 1}. #{name} "
+      binding.pry
     end
-    string
   end
 end 
