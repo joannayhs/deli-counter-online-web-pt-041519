cfe6b8acd7903b28cdcb43bca935862b8a2019b4
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 18:43:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ def line(array)
 end 
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+  puts "Welcome, #{name}. You are number #{array.index + 1} in line."
 end 
