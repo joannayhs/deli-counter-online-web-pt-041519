cb2f22a641a7607ea89678cbe4d15e87d878ff40
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-20 18:28:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   elsif array.size >= 1 
     array.each_with_index do |name, index|
-      puts "The line is currently #{index + 1}. #{name}"
+      puts "The line is currently: #{index + 1}. #{name}"
     end
   end
 end 
